aacd5aecc7372a81832962d7a9e8dbb568163c77
Ronaldo <roengsilva@yahoo.com.br>
2025-10-24 00:31:54 -0300

    feat: Adiciona nova view e adiona botão para iniciar calculos no app

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3015467/imfitplus/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3015467/imfitplus/MainActivity.kt
@@ -1,11 +1,18 @@
 package br.edu.ifsp.scl.ads.prdm.sc3015467.imfitplus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun mainActivityClique(view: View) {
+        var intent = Intent(this,  PersonalDataActivity::class.java);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3015467/imfitplus/PersonalDataActivity.kt
@@ -0,0 +1,11 @@
+package br.edu.ifsp.scl.ads.prdm.sc3015467.imfitplus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PersonalDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_personal_data)
+    }
+}
