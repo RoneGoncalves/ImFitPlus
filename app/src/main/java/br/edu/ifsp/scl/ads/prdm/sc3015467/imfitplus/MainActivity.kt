6961c8a281144648c5945931fe5702c40fa6aaa4
Ronaldo <roengsilva@yahoo.com.br>
2025-10-29 18:52:12 -0300

    feat: Finaliza activity_personal_data

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3015467/imfitplus/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3015467/imfitplus/MainActivity.kt
@@ -3,16 +3,19 @@ package br.edu.ifsp.scl.ads.prdm.sc3015467.imfitplus
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import br.edu.ifsp.scl.ads.prdm.sc3015467.imfitplus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private  val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+        setContentView(amb.root)
 
-    fun mainActivityClique(view: View) {
-        var intent = Intent(this,  PersonalDataActivity::class.java);
-        startActivity(intent);
+        amb.startBt.setOnClickListener {
+            val intent = Intent(this,  PersonalDataActivity::class.java);
+            startActivity(intent);
+        }
     }
 }
